2bf10ed5180706d5acef04918e1abe84091fc285
Michael Ellers <michael.ellers@stapleacquisitions.com>
2023-03-03 13:42:46 -0500

    connect mongodb data to frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,16 +10,32 @@ import {
 import FlexBetween from "components/FlexBetween";
 import { useDispatch } from 'react-redux';
 import { setMode } from "state";
-import profileImage from "assets/profile.jpg";
-import { useTheme } from "@mui/material";
-import { AppBar, Toolbar, InputBase, IconButton } from "@mui/material";
+import profileImage from "../assets/profile.jpeg";
+import { 
+    Box,
+    Button,
+    Typography,
+    useTheme,
+    Menu,
+    MenuItem,
+    AppBar,
+    Toolbar,
+    InputBase,
+    IconButton 
+} from "@mui/material";
 
 function Navbar({
+    user,
     isSidebarOpen,
     setIsSidebarOpen
 }) {
     const dispatch = useDispatch();
     const theme = useTheme();
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const handleClick = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
   return (
     <AppBar 
         sx={{
@@ -59,6 +75,58 @@ function Navbar({
                 <IconButton>
                     <SettingsOutlined sx={{ fontSize: "25px"}} />
                 </IconButton>
+
+                <FlexBetween>
+                    <Button 
+                        onClick={handleClick} 
+                        sx={{ 
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            textTransform: "none",
+                            gap: "1rem"
+                        }}
+                    >  
+                        <Box
+                            component="img"
+                            alt="profile"
+                            src={profileImage}
+                            height="32px"
+                            width="32px"
+                            borderRadius="50%"
+                            sx={{ objectFit: "cover" }} 
+                        />
+                        <Box textAlign="left">
+                            <Typography
+                                fontWeight="bold"
+                                fontSize="0.85rem"
+                                sx={{ color: theme.palette.secondary[100] }}
+                            >
+                                {user.name}
+                            </Typography>
+                            <Typography
+                                fontSize="0.75rem"
+                                sx={{ color: theme.palette.secondary[200] }}
+                            >
+                                {user.occupation}
+                            </Typography>
+                        </Box>
+                        <ArrowDropDownOutlined 
+                            sx={{ 
+                                color: theme.palette.secondary[300], 
+                                fontSize: "25px"
+                            }}
+                        /> 
+                    </Button>
+                    <Menu 
+                        anchorEl={anchorEl}
+                        open={isOpen}
+                        onClose={handleClose}
+                        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                    >
+                        <MenuItem onClick={handleClose}>Log Out</MenuItem>
+                    </Menu>
+                </FlexBetween>
             </FlexBetween>
         </Toolbar>
     </AppBar>
