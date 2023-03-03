9a72678c33bbaf67cac30d783795a648f04d060d
Michael Ellers <michael.ellers@stapleacquisitions.com>
2023-03-03 15:32:29 -0500

    develop products tab

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -33,6 +33,7 @@ function Navbar({
     const theme = useTheme();
 
     const [anchorEl, setAnchorEl] = useState(null);
+    const isOpen = Boolean(anchorEl);
     const handleClick = (event) => setAnchorEl(event.currentTarget);
     const handleClose = () => setAnchorEl(null);
 
