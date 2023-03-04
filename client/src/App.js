8d76f445157e00115fe08c07ad349b529ac66f96
Michael Ellers <michael.ellers@stapleacquisitions.com>
2023-03-04 14:07:18 -0500

    develop monthly tab

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from "scenes/daily";
-//import Monthly from "scenes/monthly";
+import Monthly from "scenes/monthly";
 //import Breakdown from "scenes/breakdown";
 //import Admin from "scenes/admin";
 //import Performance from "scenes/performance";
@@ -35,6 +35,7 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
